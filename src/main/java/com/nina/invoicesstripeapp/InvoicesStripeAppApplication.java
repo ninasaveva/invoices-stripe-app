b4228df8f64fc36eb695d4ac3d3f7c636ef5d217
Nina <ninasaveva6882gmail.com>
2026-05-29 16:18:03 +0200

    first commit - initial setup

---
+++ src/main/java/com/nina/invoicesstripeapp/InvoicesStripeAppApplication.java
@@ -0,0 +1,13 @@
+package com.nina.invoicesstripeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicesStripeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvoicesStripeAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nina/invoicesstripeapp/InvoicesStripeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nina.invoicesstripeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoicesStripeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
